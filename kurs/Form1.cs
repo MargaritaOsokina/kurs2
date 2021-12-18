ba56287d07099b89822b6a39c045d09272475ec7
Margarita Osokina <margarita.mobil@gmail.com>
2021-12-18 18:15:41 +0800

    Переходим на векторную скорость

--- kurs/Form1.cs
+++ kurs/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static kurs.Particle;
 
 namespace kurs
 {
@@ -14,14 +15,16 @@ namespace kurs
     {
         // собственно список, пока пустой
         List<Particle> particles = new List<Particle>();
-
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+          // picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
             // генерирую 500 частиц
-            for (var i = 0; i < 500; ++i)
+          /*  for (var i = 0; i < 500; ++i)
             {
                 var particle = new Particle();
                 // переношу частицы в центр изображения
@@ -29,34 +32,70 @@ namespace kurs
                 particle.Y = picDisplay.Image.Height / 2;
                 // добавляю список
                 particles.Add(particle);
-            }
+            }*/
         }
 
         int counter = 0; // добавлю счетчик чтобы считать вызовы функции
                          // добавил функцию обновления состояния системы
         private void UpdateState()
         {
+
             foreach (var particle in particles)
             {
-                particle.Life -= 1; // уменьшаю здоровье
-                                    // если здоровье кончилось
+                particle.Life -= 1;  // не трогаем
                 if (particle.Life < 0)
                 {
-                    // восстанавливаю здоровье
+                    // тоже не трогаем
                     particle.Life = 20 + Particle.rand.Next(100);
-                    // перемещаю частицу в центр
-                    particle.X = picDisplay.Image.Width / 2;
-                    particle.Y = picDisplay.Image.Height / 2;
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+
+                    /* это убираем
                     particle.Direction = Particle.rand.Next(360);
                     particle.Speed = 1 + Particle.rand.Next(10);
+                    */
+
+                    /* ЭТО ДОБАВЛЯЮ, тут сброс состояния частицы */
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+                    /* конец ЭТО ДОБАВЛЯЮ  */
+
+                    // это не трогаем
                     particle.Radius = 2 + Particle.rand.Next(10);
                 }
                 else
                 {
-                    // а это наш старый код
+                    /* это все убираем, тут у нас старый пересчет положения частицы в пространстве 
                     var directionInRadians = particle.Direction / 180 * Math.PI;
                     particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
                     particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                    */
+
+                    // и добавляем новый, собственно он даже проще становится, 
+                    // так как теперь мы храним вектор скорости в явном виде и его не надо пересчитывать
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500)
+                {
+                    // а у тут уже наш новый класс используем
+                    var particle = new ParticleColorful();
+                    // ну и цвета меняем
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break;
                 }
             }
         }
@@ -74,15 +113,21 @@ namespace kurs
         // ну и обработка тика таймера, тут просто декомпозицию выполнили
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState(); // каждый тик обновляем систему
+            UpdateState();
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.White);
-                Render(g); // рендерим систему
+                g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
+                Render(g);
             }
 
             picDisplay.Invalidate();
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
         }
     }
 }
--- kurs/Particle.cs
+++ kurs/Particle.cs
@@ -12,18 +12,27 @@ namespace kurs
         public float X; // X координата положения частицы в пространстве
         public float Y; // Y координата положения частицы в пространстве
 
-        public float Direction; // направление движения
-        public float Speed; // скорость перемещения
+        public float SpeedX; // скорость перемещения по оси X
+        public float SpeedY; // скорость перемещения по оси Y
         public float Life; // запас здоровья частицы
+
 
         public static Random rand = new Random();
 
         public Particle()
         {
-            Direction = rand.Next(360);
-            Speed = 1 + rand.Next(10);
+
+            // генерируем произвольное направление и скорость
+            var direction = (double)rand.Next(360);
+            var speed = 1 + rand.Next(10);
+
+            // рассчитываем вектор скорости
+            SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+            SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+            // а это не трогаем
             Radius = 2 + rand.Next(10);
-            Life = 20 + rand.Next(100); // Добавили исходный запас здоровья от 20 до 120
+            Life = 20 + rand.Next(100);
         }
 
         // добавили генератор случайных чисел
@@ -31,7 +40,7 @@ namespace kurs
         // конструктор по умолчанию будет создавать кастомную частицу
 
 
-        public void Draw(Graphics g)
+        public virtual void Draw(Graphics g)
         {
             // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
             float k = Math.Min(1f, Life / 100);
@@ -47,6 +56,37 @@ namespace kurs
             g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
 
             b.Dispose();
+        }
+        public class ParticleColorful : Particle
+        {
+            // два новых поля под цвет начальный и конечный
+            public Color FromColor;
+            public Color ToColor;
+
+            // для смеси цветов
+            public static Color MixColor(Color color1, Color color2, float k)
+            {
+                return Color.FromArgb(
+                    (int)(color2.A * k + color1.A * (1 - k)),
+                    (int)(color2.R * k + color1.R * (1 - k)),
+                    (int)(color2.G * k + color1.G * (1 - k)),
+                    (int)(color2.B * k + color1.B * (1 - k))
+                );
+            }
+
+            // ну и отрисовку перепишем
+            public override void Draw(Graphics g)
+            {
+                float k = Math.Min(1f, Life / 100);
+
+                // так как k уменьшается от 1 до 0, то порядок цветов обратный
+                var color = MixColor(ToColor, FromColor, k);
+                var b = new SolidBrush(color);
+
+                g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+                b.Dispose();
+            }
         }
     }
 }

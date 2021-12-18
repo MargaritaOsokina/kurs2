921b1b7844a23f0f8be61acff1345579d57a49b7
Margarita Osokina <margarita.mobil@gmail.com>
2021-12-18 17:08:40 +0800

    Разлетаются частицы

--- kurs/Particle.cs
+++ kurs/Particle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace kurs
@@ -24,6 +25,18 @@ namespace kurs
             Direction = rand.Next(360);
             Speed = 1 + rand.Next(10);
             Radius = 2 + rand.Next(10);
+        }
+        public void Draw(Graphics g)
+        {
+            // создали кисть для рисования
+            var b = new SolidBrush(Color.Black);
+
+            // нарисовали залитый кружок радиусом Radius с центром в X, Y
+            g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+
+            // удалили кисть из памяти, вообще сборщик мусора рано или поздно это сам сделает
+            // но документация рекомендует делать это самому
+            b.Dispose();
         }
     }
 }
